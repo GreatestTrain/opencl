c453b2940b0ec159f74b157bc00c8fc82b40c954
GreatestTrain <csr.manrique02@gmail.com>
2023-03-09 16:01:59 -0500

    Wed Mar  8 13:02:23 -05 2023

--- hello-world/main.cpp
+++ hello-world/main.cpp
@@ -1,69 +1,69 @@
+#define _USE_MATH_DEFINES
+#define CL_HPP_TARGET_OPENCL_VERSION 200
+
 #include <CL/opencl.hpp>
 #include <iostream>
+#include <vector>
+#define DATA_SIZE 10
 
-cl_device_id device;
-cl_context context;
-cl_command_queue queue;
-
-cl_program program;
-cl_kernel kernel;
-
-cl_mem buffer;
-
-void initialize() {
-    // Setup OpenCL - as many C libraries, OpenCL needs initialization
-    // cl_device_id device;
-    clGetDeviceIDs(NULL, CL_DEVICE_TYPE_CPU, 1, &device, NULL);
-    context = clCreateContext(NULL, 1, &device, NULL, NULL, NULL);
-    queue = clCreateCommandQueue(context, device, (cl_command_queue_properties)0, NULL);
-}
-
-// kernel -> calculates the Sin
-char* source = {
-    "kernel void calcSin(global float *data) {\n"
-    "   int id = get_global_id(0);\n"
-    "   data[id] = sin(data[id]);\n"
-    "}\n"
-};
-
-void createProgram() {
-    // Compile Kernel - (char* source)
-    program = clCreateProgramWithSource(context, 1, (const char**)&source, NULL, NULL);
-    clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
-    kernel = clCreateKernel(program, "calcSin", NULL);
-
-}
-
-void createBuffer() {
-    // create memory buffer - allocate memory on global memory of device (gpu)
-    buffer = clCreateBuffer(context, CL_MEM_READ_WRITE, DATA_SIZE, NULL, NULL);
-     // copy data to input (buffer)
-    clEnqueueWriteBuffer(queue, buffer, CL_FALSE, 0, DATA_SIZE, data, 0, NULL, NULL);
-
-}
+std::vector<cl::Platform> platforms;
+std::vector<cl::Device> devices;
 
 int main() {
-    initialize();
-    createProgram();
 
+    // declaring vectors
+    std::vector<float> data(DATA_SIZE);
+    for (int i = 0; i < DATA_SIZE; i++)
+    {
+        data.push_back(i * CL_M_PI);
+    }
+    std::vector<float> output(size_t(data.size()));
+    std::cout << data.size() << std::endl;
+
+    // get platforms
+    std::vector<cl::Platform> platforms;
+    cl::Platform::get(&platforms);
+
+    // get devices
+    std::vector<cl::Device> devices;
+    platforms[0].getDevices(CL_DEVICE_TYPE_GPU, &devices);
+
+    // get context
+    cl::Context context(devices);
+    cl::CommandQueue queue(context, devices[0]);
+
+    cl::Buffer inputBuff(context, CL_MEM_READ_ONLY, sizeof(float) * data.size());
+    cl::Buffer outputBuff(context, CL_MEM_WRITE_ONLY, sizeof(float) * data.size());
+
+    queue.enqueueWriteBuffer(inputBuff, CL_TRUE, 0, sizeof(float) * data.size(), data.data());
+
+    const char* kernel_str =
+        "kernel void calcSin(global const float* input, global float* output) {\n"
+        "   int id = get_global_id(0);\n"
+        "   output[id] = sin(input[id]);\n"
+        "}\n";
     
-    cl_mem buffer = clCreateBuffer(context, CL_MEM_READ_WRITE, DATA_SIZE, NULL, NULL);
-
-   
-    clEnqueueWriteBuffer(queue, buffer, CL_FALSE, 0, DATA_SIZE, data, 0, NULL, NULL);
-
+    cl::Program program(context, kernel_str);
+    // Building
+    auto err = program.build(devices);
+    cl::Kernel kernel(program, "calcSin");
+    
+    kernel.setArg(0, inputBuff);
+    kernel.setArg(1, outputBuff);
+    
     // Execute kernel
-    clSetKernelArg(kernel, 0, sizeof(buffer), &buffer);
-    size_t global_dimensions[] = {LENGTH, 0, 0};
-    clEnqueueNDRangeKernel(queue, kernel, 1, NULL, global_dimensions, NULL, 0, NULL, NULL);
+    cl::NDRange global_work_size(data.size());
+    queue.enqueueNDRangeKernel(kernel, cl::NullRange, global_work_size, cl::NullRange);
 
-    // Read the output
-    clEnqueueReadBuffer(queue, kernel, CL_FALSE, 0, sizeof(cl_float)*LENGTH, data, 0, NULL, NULL);
+    // Read results
+    queue.enqueueReadBuffer(outputBuff, CL_TRUE, 0, sizeof(float) * output.size(), output.data());
+    queue.finish();
 
-    // wait for queue to finish
+    for (int i = 0; i < data.size(); i++) {
+        std::cout << data[i] << " -> " << output[i] << std::endl;
+    }
 
-    //Finish queue
-    clFinish(queue);
+    return 0;
 }
 
 
---
+++ hello-world/main2.cpp
@@ -0,0 +1,79 @@
+#define _USE_MATH_DEFINES
+# define CL_HPP_TARGET_OPENCL_VERSION 200
+#include <CL/opencl.hpp>
+#include <cmath>
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    const int VECTOR_SIZE = 100;
+    std::vector<float> input(VECTOR_SIZE);
+    std::vector<float> output(VECTOR_SIZE);
+
+    // Initialize input vector with random values between 0 and 2*Pi
+    for (int i = 0; i < VECTOR_SIZE; i++) {
+        input[i] = static_cast<float>(rand()) / RAND_MAX * 2 * M_PI;
+    }
+
+    // Get available OpenCL platforms
+    std::vector<cl::Platform> platforms;
+    cl::Platform::get(&platforms);
+
+    // Get available OpenCL devices
+    std::vector<cl::Device> devices;
+    platforms[0].getDevices(CL_DEVICE_TYPE_GPU, &devices);
+
+    // Create OpenCL context and command queue
+    cl::Context context(devices);
+    cl::CommandQueue queue(context, devices[0]);
+
+    // Create OpenCL buffers for input and output vectors
+    cl::Buffer input_buffer(context, CL_MEM_READ_ONLY, sizeof(float) * VECTOR_SIZE);
+    cl::Buffer output_buffer(context, CL_MEM_WRITE_ONLY, sizeof(float) * VECTOR_SIZE);
+
+    // Copy input vector to input buffer
+    queue.enqueueWriteBuffer(input_buffer, CL_TRUE, 0, sizeof(float) * VECTOR_SIZE, input.data());
+
+    // Create OpenCL program from source code
+    const char* source_code = "__kernel void sin_kernel(__global const float* input, __global float* output)\n"
+        "{\n"
+        "    int i = get_global_id(0);\n"
+        "    output[i] = sin(input[i]);\n"
+        "}";
+
+    char* kernel_str = {
+        "void kernel calcSin(global float *data) {\n"
+        "   int id = get_global_id(0);\n"
+        "   data[id] = sin(data[id]);\n"
+        "}\n"
+    };
+    // cl::Program::Sources sources(, std::make_pair(source_code, strlen(source_code)));
+    // cl::Program::Sources sources(kernel_str);
+    cl::Program program(context, source_code);
+
+    // Compile OpenCL program for device
+    program.build(devices);
+
+    // Create OpenCL kernel from program
+    cl::Kernel kernel(program, "sin_kernel");
+
+    // Set kernel arguments
+    kernel.setArg(0, input_buffer);
+    kernel.setArg(1, output_buffer);
+
+    // Enqueue kernel for execution
+    cl::NDRange global_work_size(VECTOR_SIZE);
+    queue.enqueueNDRangeKernel(kernel, cl::NullRange, global_work_size, cl::NullRange);
+
+    // Copy output vector from output buffer
+    queue.enqueueReadBuffer(output_buffer, CL_TRUE, 0, sizeof(float) * VECTOR_SIZE, output.data());
+
+    
+    // Print resultskernel.setArg(0, memBuf);
+    for (int i = 0; i < VECTOR_SIZE; i++) {
+        std::cout << "sin(" << input[i] << ") = " << output[i] << std::endl;
+    }
+
+    return 0;
+}
